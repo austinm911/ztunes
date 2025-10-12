e822e8d7c280a708c64b8c820e92966784ef7c8f
Austin Mudd <austinm911@gmail.com>
2025-10-12 00:05:38 -0700

    clean up

--- auth/client.ts
+++
@@ -1,6 +0,0 @@
-import {jwt} from 'better-auth/plugins';
-import {createAuthClient} from 'better-auth/react';
-
-export const authClient = createAuthClient({
-  plugins: [],
-});
--- shared/exec.ts
+++
@@ -1,6 +0,0 @@
-import {execSync, ExecSyncOptions} from 'child_process';
-
-export function exec(command: string, options?: ExecSyncOptions) {
-  console.log(`> ${command}`);
-  return execSync(command, {stdio: 'inherit', ...options});
-}
