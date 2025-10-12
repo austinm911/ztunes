6f45bff96ad48379b06687fa7e3b16eea6071461
Austin Mudd <austinm911@gmail.com>
2025-10-12 00:50:29 -0700

    misc

--- apps/web/alchemy.run.ts
+++ apps/web/alchemy.run.ts
@@ -8,7 +8,7 @@ const app = await alchemy("web");
 
 export const frontend = await TanStackStart("website", {
 	bindings: {
-		DATABASE_URL: alchemy.secret(process.env.PG_URL!),
+		PG_URL: alchemy.secret(process.env.PG_URL!),
 		VITE_SERVER_URL: process.env.VITE_PUBLIC_SERVER!,
 	},
 	dev: {
